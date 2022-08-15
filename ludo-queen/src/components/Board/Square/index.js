00d229493ab93c7d263e80758f89dca40d7948f3
Enzo Leal <782760@HRBR.local>
2022-08-15 14:35:10 -0300

    Board layout and colors done

--- ludo-queen/src/components/Board/Square/index.js
+++ ludo-queen/src/components/Board/Square/index.js
@@ -1,9 +1,8 @@
 import styles from './Square.module.css';
 
-const Square = ({x, y}) => {
-
+const Square = ({x, y, color}) => {
   return (
-    <div className={styles.square} data-x={x} data-y={y} />
+    <div className={styles.square} data-x={x} data-y={y} style={{ background: color || ''}}/>
   );
 }
 
--- ludo-queen/src/components/Board/index.js
+++ ludo-queen/src/components/Board/index.js
@@ -1,18 +1,26 @@
 import styles from './Board.module.css';
 import Square from './Square';
+import squareColors from './Square/squareColors';
 
 const Board = () => {
   // Render all board  
   const squares = [];
 
   for(let i = 0; i < 15 * 15; i++) { 
-    squares.push({ key: i, x: i % 15, y: Math.floor(i / 15) });
+    const key = i;
+    const x =  i % 15;
+    const y = Math.floor(i / 15);
+    const color = squareColors[`${x},${y}`] || '';
+    
+    squares.push({ key, x, y, color });
   }
 
   return (
+    <>
     <section className={styles.board}>
-      {squares.map(({ key, x, y }) => <Square key={key} x={x} y={y} />)}
+      {squares.map(({ key, x, y, color }) => <Square key={key} x={x} y={y} color={color}/>)}
     </section>
+    </>
   );
 }
 
